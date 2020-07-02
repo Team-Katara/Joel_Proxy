80dc9355b5edfe8756c01a4dc0308636e7f02f0c
Tokki0401 <gyuhun95@gmail.com>
2020-07-01 21:02:08 -0700

    Renders ImageBrowser, Similar Items, and Review modules

--- server/index.js
+++ server/index.js
@@ -5,11 +5,13 @@ const path = require('path');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, '../public')));
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 const port = 5000;
